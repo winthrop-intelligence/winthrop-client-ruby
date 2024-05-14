8db3b7943ef88f5a3a6785d732552d09e64ad8c2
khanrazee <raxeekh+github@gmail.com>
2024-05-15 03:48:36 +0500

    removed autogen file changes

--- lib/winthrop-client-ruby.rb
+++ lib/winthrop-client-ruby.rb
@@ -14,7 +14,6 @@ Generator version: 7.4.0
 require 'winthrop-client-ruby/api_client'
 require 'winthrop-client-ruby/api_error'
 require 'winthrop-client-ruby/version'
-require 'winthrop-client-ruby/refresh_token'
 require 'winthrop-client-ruby/configuration'
 
 # Models
